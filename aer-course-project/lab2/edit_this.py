fixes error with euler angle computation, circle following works now

aer-course-project/lab2/edit_this.py
@@ -134,18 +134,18 @@ class GeoController():
         print("target velocity", target_vel, " actual velocity = ", cur_vel)
         print("target position", target_pos, " actual position = ", cur_pos)
 
-        Kp = np.diag([0.1,0.1,4])
-        Kv = np.diag([0.25,0.25,0.5])
+        Kp = np.diag([0.1,0.1,10])
+        Kv = np.diag([3,3,1])
 
         pos_e = target_pos - cur_pos
         vel_e = target_vel - cur_vel
 
         # max position error - 2 m
-        max_pos_e = 1
-        pos_e = max_pos_e * np.tanh(pos_e / max_pos_e)
+        # max_pos_e = 2
+        # pos_e = max_pos_e * np.tanh(pos_e / max_pos_e)
 
-        max_vel_e = 1
-        vel_e = max_vel_e * np.tanh(pos_e / max_vel_e)
+        # max_vel_e = 2
+        # vel_e = max_vel_e * np.tanh(pos_e / max_vel_e)
 
 
         #TODO - cap position and vel errors
@@ -161,11 +161,8 @@ class GeoController():
         desired_thrust = self.mass * LA.norm(desired_acc)
 
         #---------Task 3: Compute the desired attitude command--------#
-        # desired_yaw = 0
-
-
-
-        desired_yaw = np.arctan2(target_vel[1], target_vel[0]) - np.pi/2 #atan(py/px) + pi/2
+        desired_yaw = 0
+        # desired_yaw = np.arctan2(target_vel[1], target_vel[0]) - np.pi/2 #atan(py/px) + pi/2
         psi = desired_yaw #self.last_rpy[2]
         x_c = np.array([np.cos(psi), np.sin(psi), 0] )
         y_c = np.array([-np.sin(psi), np.cos(psi),  0] )
@@ -178,7 +175,8 @@ class GeoController():
         y_b_des = y_b_des /LA.norm(y_b_des)
 
         # compute desired roll and pitch
-        R = np.concatenate((x_b_des, y_b_des, z_b_des)).reshape((3,3))
+        # R = np.concatenate((x_b_des, y_b_des, z_b_des)).reshape((3,3))
+        R = np.column_stack([x_b_des, y_b_des, z_b_des])
         # print("Rotation matrix shape: ", R.shape)
         # print("Rotation matrix R=",R)
         # a_actual = desired_thrust / self.mass  - self.grav * np.array([0,0,1])
@@ -188,23 +186,25 @@ class GeoController():
         # az = a_actual[2]
         # desired_roll = np.arctan2(az, ay)
         # desired_pitch = np.arctan2(az, ax)
-        desired_roll = -np.arcsin(-R[1,2]) # arcsin(-R_23)
-        desired_pitch = np.arctan2(R[1,0], R[1,1]) #arctan(R_21/R_22)
+        # desired_roll = -np.arcsin(-R[1,2]) # arcsin(-R_23)
+        # desired_pitch = np.arctan2(R[1,0], R[1,1]) #arctan(R_21/R_22)
+
+        desired_euler = Rotation.from_matrix(R).as_euler('xyz', degrees=False)
 
         # desired_roll = 0
         # desired_pitch = 0
 
-        print("pitch=", desired_pitch*180/np.pi)
-        print("roll=", desired_roll*180/np.pi)
-        print("yaw=", desired_yaw*180/np.pi)
+        print("pitch=", desired_euler[0]*180/np.pi)
+        print("roll=", desired_euler[1]*180/np.pi)
+        print("yaw=", desired_euler[2]*180/np.pi)
 
         # print("pos_e=",pos_e)
         # print("vel_e=",vel_e)
 
         # desired_euler = np.array([desired_pitch, desired_roll, desired_yaw]).reshape((3,))
-        desired_euler[0] = desired_roll
-        desired_euler[1] = desired_pitch
-        desired_euler[2] = desired_yaw
+        # desired_euler[0] = desired_roll
+        # desired_euler[1] = desired_pitch
+        # desired_euler[2] = desired_yaw
 
     
         return desired_thrust, desired_euler, pos_e
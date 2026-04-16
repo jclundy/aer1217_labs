ddc60241121d9a7c9b988a0252af4694caafe79a
Joseph Lundy <joseph.lundy@mail.utoronto.ca>
2026-04-16 15:28:00 -0400

    wip on casadi optimizer

---
+++ aer-course-project/casadi_optimizer.py
@@ -0,0 +1,249 @@
+
+# from casadi import *
+import casadi as ca
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class CasadiSolver:
+    def __init__(self, waypoints,durations):
+        self.durations = durations
+        self.waypoints = waypoints
+        N = waypoints.shape[0] - 1
+        self.N = N
+        self.A3 = ca.SX.sym('A3', N)
+        self.B3 = ca.SX.sym('B3', N)
+        self.C3 = ca.SX.sym('C3', N)
+
+        self.frac = ca.SX.sym('t_frac', N)
+        self.total_duration = ca.SX.sym('T')
+
+        self.max_accel_xy = 5
+        self.max_accel_z = 0.517
+        self.dt = 1/60
+
+        self.max_jerk_xy = 2 * self.max_accel_xy / self.dt
+        self.max_jerk_z = 2 * self.max_accel_z / self.dt
+
+        total_duration = np.sum(durations)
+        fractions = durations / total_duration
+
+        x_error = position_error_1d(self.A3, waypoints[:,0], fractions)
+
+        y_error = position_error_1d(self.B3, waypoints[:,1], fractions)
+
+        z_error = position_error_1d(self.C3, waypoints[:,2], fractions)
+
+        cost = ca.sum(x_error **2) + ca.sum(y_error**2) + ca.sum(z_error**2)
+
+        # jerk_integral = 36 * ca.sum(self.A3**2 * self.times)
+
+
+        opt_variables = ca.vertcat(ca.reshape(self.A3, -1, 1), ca.reshape(self.B3, -1, 1), ca.reshape(self.C3, -1, 1))
+
+        g = []
+        g.append(self.A3)
+        g.append(self.B3)
+        g.append(self.C3)
+
+        # lb = np.zeros(4 * N + 1)
+        # # total time lb
+        # lb[0] = 20
+        # # fraction lower bound
+        # lb[1:N+1] = 0
+        # # A3 lower bound
+        # lb[N+1:2*N+1] = -self.max_jerk_xy 
+        # # B3 lower bound
+        # lb[2*N+1:3*N+1] = -self.max_jerk_xy
+        # # C3 lower bound
+        # lb[2*N+1:3*N+1] = -self.max_jerk_xz
+
+
+        self.lbg = -self.max_jerk_xy
+        self.ubg = self.max_jerk_xy
+
+        opt_constraints = ca.vertcat(*g)
+
+        # p - equality constraints
+        nlp_prob = {'f': cost, 'x': opt_variables, 'g': opt_constraints}
+        opts = {
+            'ipopt.print_level': 0,
+            'print_time': 0,
+            'ipopt.max_iter': 50,
+            'ipopt.tol': 1e-4,
+            'jit': True,
+            'compiler': 'shell',
+            'jit_options': {
+                'flags': ['-O3'],
+                'verbose': False
+            }
+        }
+
+        self.solver = ca.nlpsol('solver', 'ipopt', nlp_prob, opts)
+    
+    def solve_coefficients(self,initial_guess):
+        solution = self.solver(x0 = initial_guess, lbg=self.lbg, ubg=self.ubg)
+        coeffs = solution['x'].reshape((-1,3))
+
+        A3 = coeffs[:,0]
+        B3 = coeffs[:,1]
+        C3 = coeffs[:,2]
+
+        return A3, B3, C3, solution
+
+
+
+def position_error_1d(X,waypoints, times):
+    P3 = X
+
+    n = waypoints.shape[0] - 1
+
+    M = np.zeros((n,n))
+    M[1:,:] = np.tril(np.ones((n-1,n)))
+
+    # P2 = 3 * M @ (P3 * times)
+    P2 = 3 * ca.mtimes([M, P3]) * times
+
+    # P1 = 2 * M @ (P2 * times) + 3 * M @ (P3 * times **2)
+    P1 = 2 * ca.mtimes(M, P2) * times + 3* ca.mtimes(M, P3) * times **2
+
+    # P0 = M @ (P1 * times) + M @ (P2 * times **2) + M @ (P3 * times **3) + waypoints[0]
+    P0 = ca.mtimes(M, P1) * times + ca.mtimes(M, P2) * times**2 + ca.mtimes(M, P3) * times**3 + waypoints[0]
+
+    # p = P0 + P1 * times + P2 * times**2 + P3 * times**3
+    p = P0 + P1 * times + P2*times**2 + P3 * times**3
+
+    # last velocity is zero
+    # pd = P1 + 2 * P2 * times + 3 * P3 * times**2
+    # pdd = 2 * P2 + 6 * P3 * times
+
+    # Velocity equality constraint
+
+    # pd1 = np.insert(P1[1:], n-1,0)
+
+    eqn_p1 = p - waypoints[1:]
+
+    # eqn_pd = pd - pd1
+
+    # pdd1 = np.insert(2*P2[1:], n-1,0)
+
+    # eqn_pdd = pdd - pdd1
+
+    return eqn_p1
+
+def generate_waypoints():
+    poses = []
+
+    poses = [[-1.0, -3.0, 1.0], 
+            [-0.09999980975910072, -2.49952220397356, 1.0], 
+            [0.5, -2.5, 1.0], 
+            [2.0, -2.1, 1.0]]
+    return np.array(poses).reshape(-1,3)
+
+def unwind_coefficients(A3, B3, C3, times, waypoints):
+    
+    n = times.shape[0]
+
+    M = np.zeros((n,n))
+    M[1:,:] = np.tril(np.ones((n-1,n)))
+
+    A2 = 3 * M @ (A3 * times)
+    B2 = 3 * M @ (B3 * times)
+    C2 = 3 * M @ (C3 * times)
+
+    A1 = 2 * M @ (A2 * times) + 3 * M @ (A3 * times **2)
+    B1 = 2 * M @ (B2 * times) + 3 * M @ (B3 * times **2)
+    C1 = 2 * M @ (C2 * times) + 3 * M @ (C3 * times **2)
+
+    A0 = M @ (A1 * times) + M @ (A2 * times **2) + M @ (A3 * times **3) + waypoints[0,0]
+    # A0 = self.waypoints[0:n,0]       
+
+    B0 = M @ (B1 * times) + M @ (B2 * times **2) + M @ (B3 * times **3) + waypoints[0,1]
+    # B0 = self.waypoints[0:n,1]
+
+    C0 = M @ (C1 * times) + M @ (C2 * times **2) + M @ (C3 * times **3) + waypoints[0,2]
+    # C0 = self.waypoints[0:n,2]
+
+    Ai = np.concatenate([A0, A1, A2, A3]).reshape(-1,4)
+    Bi = np.concatenate([B0, B1, B2, B3]).reshape(-1,4)
+    Ci = np.concatenate([C0, C1, C2, C3]).reshape(-1,4)                
+    """
+    inputs: 3rd order polynomials
+    outputs:
+    """
+    A0 = np.array(A0).flatten()
+    A1 = np.array(A1).flatten()
+    A2 = np.array(A2).flatten()
+    A3 = np.array(A3).flatten()
+    B0 = np.array(B0).flatten()
+    B1 = np.array(B1).flatten()
+    B2 = np.array(B2).flatten()
+    B3 = np.array(B3).flatten()
+    C0 = np.array(C0).flatten()
+    C1 = np.array(C1).flatten()
+    C2 = np.array(C2).flatten()
+    C3 = np.array(C3).flatten()
+    return A0, A1, A2, A3, B0, B1, B2, B3, C0, C1, C2, C3
+
+def main():
+    waypoints = generate_waypoints()
+    print("waypoints=", waypoints.reshape(-1,3))
+
+    max_time = 60
+    n = waypoints.shape[0]-1
+
+    p_prev = waypoints[0:n,:]
+    p_next = waypoints[1:,:]
+    waypoint_min_lengths = np.linalg.norm(p_next - p_prev, axis=1)
+
+    print("waypoint_min_lengths=",waypoint_min_lengths.reshape(1,-1))
+
+    total_length = np.sum(waypoint_min_lengths)
+    durations = waypoint_min_lengths / total_length * max_time
+
+    fractions = durations / max_time
+
+    print("fractions=", fractions)
+
+
+    solver = CasadiSolver(waypoints,fractions)
+
+    X0 = np.zeros(n*3)
+    A3, B3, C3, solution = solver.solve_coefficients(X0)
+
+
+    print("solution", solution)
+    print("A3", A3)
+    print("B3", B3)
+    print("C3", C3)
+
+
+
+    wx = waypoints[:,0]
+    wy = waypoints[:,1]
+    wz = waypoints[:,2]
+
+    A0, A1, A2, A3, B0, B1, B2, B3, C0, C1, C2, C3 = unwind_coefficients(A3, B3, C3, fractions, waypoints)
+
+    tn = fractions[n-1]
+    xn = A0[n-1] + A1[n-1] * tn + A2[n-1] * tn**2 + A3[n-1] * tn**3
+    yn = B0[n-1] + B1[n-1] * tn + B2[n-1] * tn**2 + B3[n-1] * tn**3
+    zn = C0[n-1] + C1[n-1] * tn + C2[n-1] * tn**2 + C3[n-1] * tn**3
+
+    A0_plus = np.concatenate([np.array(A0).flatten(), np.array([xn])])
+    B0_plus = np.concatenate([np.array(B0).flatten(), np.array([yn])])
+    C0_plus = np.concatenate([np.array(C0).flatten(), np.array([zn])])
+
+    print("A0_plus", A0_plus)
+    print("B0_plus", B0_plus)
+    print("C0_plus", C0_plus)
+    print("waypoints=", waypoints.reshape(-1,3))
+
+    ax0 = plt.figure().add_subplot(projection='3d')
+    ax0.scatter(A0_plus, B0_plus, C0_plus)
+    ax0.scatter(wx, wy, wz)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()

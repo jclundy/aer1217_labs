c1126b3c06de4201abb367edff8d3aab21052dec
jclundy <joseph.lundy456@gmail.com>
2026-04-22 14:10:25 -0400

    fixes simulation stop

--- aer-course-project/edit_this_RRT.py
+++ aer-course-project/edit_this_RRT.py
@@ -160,7 +160,7 @@ class Controller():
 
         ref_state = None
         save_file = "test_states.npz" # "trajectory_states.npz"
-        recompute_trajectory = True
+        recompute_trajectory = False
         if(os.path.exists(save_file) and not recompute_trajectory):
             print("loading saved trajectory")
             npzfile = np.load(save_file)
@@ -254,7 +254,7 @@ class Controller():
             command_type, args = Command(3), [0., 3]  # land
         elif iteration >= end_iteration:
             print("sending exit command")
-            command_type, args = Command(-1), []  # exit
+            command_type, args = Command(4), []  # exit
         else:
             command_type, args = Command(0), []
         #########################
